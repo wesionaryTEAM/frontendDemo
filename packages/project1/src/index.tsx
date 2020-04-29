be3edd599fbeb549b39c9f54fddbecd10fd1b893
Aiska Basnet <aiska.basnet@gmail.com>
2020-04-29 12:36:07 +0545

    Added antd

--- packages/project1/src/index.tsx
+++ packages/project1/src/index.tsx
@@ -2,7 +2,7 @@ import './i18n';
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import 'antd/dist/antd.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
